fb128d403dce0802b123175b2997b24b1525f1bf
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-24 22:12:31 +0200

    Unnecessary code from template removed.

--- Formula1WebApplication/Controllers/HomeController.cs
+++ Formula1WebApplication/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Formula1WebApplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
