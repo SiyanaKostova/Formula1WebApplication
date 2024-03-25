80824af297ad9b99f36ebd13c9d042a5d43cfde3
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-25 10:59:02 +0200

    Added constants for minimum and maximum length of properties.

---
+++ Formula1WebApplication.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace HouseRentingSystem.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int OrganizerPhoneNumberMinLength = 7;
+        public const int OrganizerPhoneNumberMaxLength = 15;
+
+        public const int PilotNameMaxLength = 50;
+        public const int PilotNationalityMaxLength = 50;
+        public const int PilotTeamNameMaxLength = 50;
+        public const int PilotBiographyMinLength = 50;
+        public const int PilotBiographyMaxLength = 500;
+
+        public const int RaceNameMaxLength = 50;
+        public const int RaceLocationMinLength = 10;
+        public const int RaceLocationMaxLength = 100;
+        public const int RaceCircuitInfoMinLength = 10;
+        public const int RaceCircuitInfoMaxLength = 200;
+
+        public const int EventNameMaxLength = 50;
+        public const int EventDescriptionMinLength = 20;
+        public const int EventDescriptionMaxLength = 500;
+        public const int EventLocationMinLength = 10;
+        public const int EventLocationMaxLength = 100;
+
+        public const int NewsArticleTitleMaxLength = 50;
+        public const int NewsArticleDescriptionMinLength = 30;
+        public const int NewsArticleDescriptionMaxLength = 2000;
+    }
+}
