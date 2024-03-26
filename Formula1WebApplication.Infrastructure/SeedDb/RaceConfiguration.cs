73811a6187b514c359736ae2ea96cef8ab83f501
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 22:04:28 +0200

    Race Configuration created.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/RaceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
+using Formula1WebApplication.Infrastructure.Data.SeedDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.SeedDb
+{
+    internal class RaceConfiguration : IEntityTypeConfiguration<Race>
+    {
+        public void Configure(EntityTypeBuilder<Race> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Race[] {data.BahrainGrandPrix, data.SaudiArabiaGrandPrix, data.AustraliaGrandPrix});
+        }
+    }
+}
