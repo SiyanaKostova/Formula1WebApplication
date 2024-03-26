c31ad2a41858bdc30c3b0928fcfcdb44a5b1cd71
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 23:13:28 +0200

    News Article Configuration added.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/Configurations/NewsArticleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
+using Formula1WebApplication.Infrastructure.Data.SeedDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.SeedDb.Configurations
+{
+    internal class NewsArticleConfiguration : IEntityTypeConfiguration<NewsArticle>
+    {
+        public void Configure(EntityTypeBuilder<NewsArticle> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new NewsArticle[] { data.TheStrategist, data.NotWhereIExpectedToBe, data.HinchsHeroes });
+        }
+    }
+}
