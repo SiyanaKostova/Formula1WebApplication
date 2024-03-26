2200180c4b1a8f65f803ba0e8041d851cdb30178
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 19:44:42 +0200

    User Configuration added.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.Data.SeedDb
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new IdentityUser[] { data.OrganizerUser, data.GuestUser });
+        }
+    }
+}
