45ec2b1994d316aeb5ab79c7d5be2d003c481488
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 19:44:29 +0200

    Organizer Configuration added.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/OrganizerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.Data.SeedDb
+{
+    internal class OrganizerConfiguration : IEntityTypeConfiguration<Organizer>
+    {
+        public void Configure(EntityTypeBuilder<Organizer> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Organizer[] { data.Organizer });
+        }
+    }
+}
