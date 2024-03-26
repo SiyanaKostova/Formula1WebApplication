f804d831dd6cdfb7c87ac8f3c319050e00c1d1a5
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 21:31:14 +0200

    Pilot Configuration created.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/PilotConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
+using Formula1WebApplication.Infrastructure.Data.SeedDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.SeedDb
+{
+    internal class PilotConfiguration : IEntityTypeConfiguration<Pilot>
+    {
+        public void Configure(EntityTypeBuilder<Pilot> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Pilot[] { data.MaxVerstappen, data.CharlesLeclerc, data.SergioPerez,
+                data.CarlosSainz, data.OscarPiastri, data.LandoNorris, data.GeorgeRussell, data.FernandoAlonso,
+                data.LanceStroll, data.LewisHamilton, data.YukiTsunoda, data.NicoHulkenberg, data.KevinMagnussen,
+                data.AlexAlbon, data.GuanyuZhou, data.DanielRicciardo, data.EstebanOcon, data.PierreGasly,
+                data.ValtteriBottas, data.LoganSargeant});
+        }
+    }
+}
