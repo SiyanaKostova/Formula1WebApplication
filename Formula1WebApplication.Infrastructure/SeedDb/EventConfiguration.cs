5fc538f42b4d39f89429f66f047ddce37f695089
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-26 22:51:51 +0200

    Event Configuration created.

---
+++ Formula1WebApplication.Infrastructure/SeedDb/EventConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
+using Formula1WebApplication.Infrastructure.Data.SeedDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Formula1WebApplication.Infrastructure.SeedDb
+{
+    internal class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Event[] { data.MeetAndGreet, data.AutographSession, data.ExclusivePreview });
+        }
+    }
+}
