c486b32d1fa9c1b35522d437e611c2ccdf8e191b
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 00:14:56 +0300

    Become Organizer implemented in Controller.

--- Formula1WebApplication/Controllers/OrganizerController.cs
+++ Formula1WebApplication/Controllers/OrganizerController.cs
@@ -1,13 +1,22 @@
-﻿using Formula1WebApplication.Core.Models.Organizer;
+﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Core.Models.Organizer;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static Formula1WebApplication.Core.Constants.MessageConstants;
 
 namespace Formula1WebApplication.Controllers
 {
-	public class OrganizerController : BaseController
+    public class OrganizerController : BaseController
 	{
-		[HttpGet]
-		[HttpPost]
-		public async Task<IActionResult> Become()
+        private readonly IOrganizerService organizerService;
+
+        public OrganizerController(IOrganizerService _organizerService)
+        {
+            organizerService = _organizerService;
+        }
+
+        [HttpGet]
+		public IActionResult Become()
 		{
 			var model = new BecomeOrganizerFormModel();
 
@@ -17,7 +26,34 @@ namespace Formula1WebApplication.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Become(BecomeOrganizerFormModel model)
 		{
-			return RedirectToAction(nameof(PilotController.All), "Pilot");
-		}
-	}
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userId = User.Id(); 
+
+            if (await organizerService.IsUserAlreadyOrganizerAsync(userId))
+            {
+                ModelState.AddModelError(string.Empty, AlreadyOrganizerMessage);
+                return View(model);
+            }
+
+            if (await organizerService.IsPhoneNumberAlreadyUsedAsync(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(BecomeOrganizerFormModel.PhoneNumber), PhoneExistsMessage);
+                return View(model);
+            }
+
+            var success = await organizerService.BecomeOrganizerAsync(userId, model.PhoneNumber);
+
+            if (success)
+            {
+                return RedirectToAction(nameof(NewsArticleController.All), "NewsArticle");
+            }
+
+            ModelState.AddModelError(string.Empty, "An unknown error occurred. Please try again.");
+            return View(model);
+        }
+    }
 }
