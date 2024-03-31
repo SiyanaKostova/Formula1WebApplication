0a17c096444eb4d7d3d5da077f40a6cf3cccca23
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 13:35:57 +0300

    Home Controller edited.

--- Formula1WebApplication/Controllers/HomeController.cs
+++ Formula1WebApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Formula1WebApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace Formula1WebApplication.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
