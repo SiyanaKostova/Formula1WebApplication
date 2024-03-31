a5cf8b85b0547eb495d3c2361349a2f545449254
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 13:57:27 +0300

    Created message constants.

---
+++ Formula1WebApplication.Core/Constants/MessageConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Formula1WebApplication.Core.Constants
+{
+	public static class MessageConstants
+	{
+		public const string RequiredMessage = "The {0} field is required";
+
+		public const string LengthMessage = "The {0} field must be between {2} and {1} characters long";
+
+		public const string PhoneExistsMessage = "Phone number already exists. Enter another one";
+
+		public const string NoRentsMessage = "You should have no rents to become an organizer";
+	}
+}
