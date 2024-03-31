cab60701cc47f84f4c8f5049cb12e3993c96777c
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 13:27:23 +0300

    Service model for News Article implemented.

---
+++ Formula1WebApplication.Core/Models/Home/NewsArticleIndexServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Formula1WebApplication.Core.Models.Home
+{
+    public class NewsArticleIndexServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
