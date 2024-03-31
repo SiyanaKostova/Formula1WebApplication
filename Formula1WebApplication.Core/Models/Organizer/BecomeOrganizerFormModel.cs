2626f4a6259c79774c70cf9fbcd4ef863d7f76c2
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 13:57:59 +0300

    Become Organizer Form Model implemented.

---
+++ Formula1WebApplication.Core/Models/Organizer/BecomeOrganizerFormModel.cs
@@ -0,0 +1,17 @@
+﻿using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+using static Formula1WebApplication.Core.Constants.MessageConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Formula1WebApplication.Core.Models.Organizer
+{
+	public class BecomeOrganizerFormModel
+	{
+		[Required(ErrorMessage = RequiredMessage)]
+		[StringLength(OrganizerPhoneNumberMaxLength,
+			MinimumLength = OrganizerPhoneNumberMinLength,
+			ErrorMessage = LengthMessage)]
+		[Display(Name = "Phone number")]
+		[Phone]
+		public string PhoneNumber { get; set; } = null!;
+	}
+}
