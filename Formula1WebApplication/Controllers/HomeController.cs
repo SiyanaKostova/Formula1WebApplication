98aa25b3f5e0b44061cc398a78f5da92ce35e1e9
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 18:19:47 +0300

    Last Three Articles Method created.

--- Formula1WebApplication/Controllers/HomeController.cs
+++ Formula1WebApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Formula1WebApplication.Models;
+﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,16 +9,22 @@ namespace Formula1WebApplication.Controllers
     public class HomeController : BaseController
 	{
         private readonly ILogger<HomeController> _logger;
+        private readonly INewsArticleService newsArticleService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            INewsArticleService _newsArticleService)
         {
             _logger = logger;
+            newsArticleService = _newsArticleService;
         }
 
         [AllowAnonymous]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = await newsArticleService.LastThreeNewsArticlesAsync();
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
