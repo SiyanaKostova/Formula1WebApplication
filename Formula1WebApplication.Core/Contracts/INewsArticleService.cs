5177c2a74eac942ed5d21a3c472be3aa4dedf5dc
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 18:20:48 +0300

    Last Three Houses implemented in News Article Service.

--- Formula1WebApplication.Core/Contracts/INewsArticleService.cs
+++ Formula1WebApplication.Core/Contracts/INewsArticleService.cs
@@ -1,6 +1,9 @@
-﻿namespace Formula1WebApplication.Core.Contracts
+﻿using Formula1WebApplication.Core.Models.Home;
+
+namespace Formula1WebApplication.Core.Contracts
 {
     public interface INewsArticleService
     {
+        Task<IEnumerable<NewsArticleIndexServiceModel>> LastThreeNewsArticlesAsync();
     }
 }
--- Formula1WebApplication.Core/Services/NewsArticleService.cs
+++ Formula1WebApplication.Core/Services/NewsArticleService.cs
@@ -1,8 +1,33 @@
 ﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Core.Models.Home;
+using Formula1WebApplication.Infrastructure.Common;
+using Formula1WebApplication.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Formula1WebApplication.Core.Services
 {
     public class NewsArticleService : INewsArticleService
     {
+        private readonly IRepository repository;
+
+        public NewsArticleService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<NewsArticleIndexServiceModel>> LastThreeNewsArticlesAsync()
+        {
+            return await repository
+                .AllReadOnly<NewsArticle>()
+                .OrderByDescending(n => n.Id)
+                .Take(3)
+                .Select(n => new NewsArticleIndexServiceModel()
+                {
+                    Id = n.Id,
+                    Title = n.Title,
+                    ImageUrl = n.ImageUrl
+                })
+                .ToListAsync();
+        }
     }
 }
