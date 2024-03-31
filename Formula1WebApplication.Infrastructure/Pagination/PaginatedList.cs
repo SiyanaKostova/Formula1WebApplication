8cda847d58194241c710e0aa9013c82d1dfc1c25
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-31 21:04:58 +0300

    Pagination logic implemented.

---
+++ Formula1WebApplication.Infrastructure/Pagination/PaginatedList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Formula1WebApplication.Infrastructure.Pagination
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public int TotalCount { get; private set; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            TotalCount = count;
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+    }
+}
