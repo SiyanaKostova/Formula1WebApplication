063bbaa1bc1c3d8555310df436c6efb2a3cec025
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 12:56:38 +0300

    Added message for maximum length.

--- Formula1WebApplication.Core/Constants/MessageConstants.cs
+++ Formula1WebApplication.Core/Constants/MessageConstants.cs
@@ -6,6 +6,8 @@
 
 		public const string LengthMessage = "The {0} field must be between {2} and {1} characters long";
 
+		public const string MaxLengthMessage = "The {0} field must be less than {1} characters long";
+
 		public const string PhoneExistsMessage = "Phone number already exists. Enter another one";
 
 		public const string AlreadyOrganizerMessage = "You are already an organizer.";
