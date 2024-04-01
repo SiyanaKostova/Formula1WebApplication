2b38dcab059c5456c1158397fb343b91322f17cc
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 12:55:44 +0300

    Pilot Service Model created.

---
+++ Formula1WebApplication.Core/Models/Pilot/PilotServiceModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Formula1WebApplication.Core.Constants.MessageConstants;
+using static Formula1WebApplication.Infrastructure.Constants.DataConstants;
+
+namespace Formula1WebApplication.Core.Models.Pilot
+{
+    public class PilotServiceModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PilotNameMaxLength,
+        ErrorMessage = MaxLengthMessage)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PilotNameMaxLength,
+        ErrorMessage = MaxLengthMessage)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Display(Name = "Nationality")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PilotNationalityMaxLength,
+        ErrorMessage = MaxLengthMessage)]
+        public string Nationality { get; set; } = string.Empty;
+
+        [Display(Name = "Team")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PilotTeamNameMaxLength,
+        ErrorMessage = MaxLengthMessage)]
+        public string TeamName { get; set; } = string.Empty;
+
+        [Display(Name = "Biography")]
+        [StringLength(PilotBiographyMaxLength,
+        MinimumLength = PilotBiographyMinLength,
+        ErrorMessage = LengthMessage)]
+        public string Biography { get; set; } = string.Empty;
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; } = string.Empty;
+    }
+}
