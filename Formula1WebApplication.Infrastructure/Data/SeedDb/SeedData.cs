fbdefbcac62a08c4fa8f5ce403ba72fbe6dd46b2
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 20:07:15 +0300

    ImagePath of Pilots changed.

--- Formula1WebApplication.Infrastructure/Data/SeedDb/SeedData.cs
+++ Formula1WebApplication.Infrastructure/Data/SeedDb/SeedData.cs
@@ -102,7 +102,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Dutch",
                 TeamName = "Red Bull Racing",
                 Biography = "He’s Max by name, and max by nature. Arriving as Formula 1’s youngest ever competitor at just 17 years old, Verstappen pushed his car, his rivals and the sport’s record books to the limit. The baby-faced Dutchman with the heart of a lion took the Toro Rosso – and then the Red Bull – by the horns with his instinctive racing style.",
-                ImagePath = "\\wwwroot\\images\\pilots\\maxverstappen.png"
+                ImagePath = "/images/pilots/maxverstappen.png"
             };
 
             CharlesLeclerc = new Pilot
@@ -113,7 +113,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Monegasque",
                 TeamName = "Ferrari",
                 Biography = "Leclerc arrived in F1 on a tidal wave of expectation. He showcased a dazzling array of skills from scorching pole positions, commanding victories – even when his car caught fire twice at Silverstone – to an ability to muscle his way through the pack. Winning back-to-back championships also taught Leclerc how to handle pressure, another useful tool in Formula 1 racing.",
-                ImagePath = "\\wwwroot\\images\\pilots\\charlesleclerc.png"
+                ImagePath = "/images/pilots/charlesleclerc.png"
             };
 
             SergioPerez = new Pilot
@@ -124,7 +124,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Mexican",
                 TeamName = "Red Bull Racing",
                 Biography = "He’s the fighter with a gentle touch from the land of the Lucha Libre.Perez’s reputation in F1 has been built on opposite approaches to Grand Prix racing. On the one hand, he is a punchy combatant who wrestles his way through the pack and into the points. Never afraid to add a bit of spice to his on-track encounters, even his team mates don’t always escape the Mexican’s heat.",
-                ImagePath = "\\wwwroot\\images\\pilots\\sergioperez.png"
+                ImagePath = "/images/pilots/sergioperez.png"
             };
 
             CarlosSainz = new Pilot
@@ -135,7 +135,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Spanish",
                 TeamName = "Ferrari",
                 Biography = "He’s the matador from Madrid racing royalty. After entering F1’s Bull Ring paired alongside Max Verstappen at Toro Rosso in 2015, Sainz quickly showed his fighting spirit. A tenacious racer, he puts the car on the edge as he hustles his way through the pack. No wonder Sainz has earned the nickname Chilli.",
-                ImagePath = "\\wwwroot\\images\\pilots\\carlossainz.png"
+                ImagePath = "/images/pilots/carlossainz.png"
             };
 
             OscarPiastri = new Pilot
@@ -146,7 +146,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Australian",
                 TeamName = "McLaren",
                 Biography = "Born in Melbourne, just a stone’s throw away from the Australian Grand Prix venue, a young Oscar Piastri’s dreams of one day racing in Formula 1 were ignited by the sport’s star drivers roaring around his local streets, otherwise known as Albert Park.",
-                ImagePath = "\\wwwroot\\images\\pilots\\oscarpiastri.png"
+                ImagePath = "/images/pilots/oscarpiastri.png"
             };
 
             LandoNorris = new Pilot
@@ -157,7 +157,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "British",
                 TeamName = "McLaren",
                 Biography = "He has flair and fighting spirit in bountiful supply. McLaren had the British teenager on their books for two years before fast-tracking him into F1’s galaxy of stars in 2019. A firecracker in his junior career, with a penchant for pole positions and wheel-to-wheel tussles, Norris didn’t let them down.",
-                ImagePath = "\\wwwroot\\images\\pilots\\landonorris.png"
+                ImagePath = "/images/pilots/landonorris.png"
             };
 
             GeorgeRussell = new Pilot
@@ -168,7 +168,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "British",
                 TeamName = "Mercedes",
                 Biography = "He’s the driver with the motto: “If in doubt, go flat out”. George Russell has lived by it throughout his F1 career to date, out-qualifying seasoned team mate Robert Kubica at all 21 Grands Prix in his rookie season, putting Williams back on the podium in 2021, and landing his first race win with Mercedes in 2022.",
-                ImagePath = "\\wwwroot\\images\\pilots\\georgerussel.png"
+                ImagePath = "/images/pilots/georgerussel.png"
             };
 
             FernandoAlonso = new Pilot
@@ -179,7 +179,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Spanish",
                 TeamName = "Alpine",
                 Biography = "Fiercely competitive, Alonso is not shy about his talent, rating himself as 9/10 “in everything”, and few in the know would disagree, with his performances in F1 characterised by blistering speed, brilliant tactical thinking, exemplary race craft, a razor-sharp eye for detail and a relentless determination to win.",
-                ImagePath = "\\wwwroot\\images\\pilots\\fernandoalonso.png"
+                ImagePath = "/images/pilots/fernandoalonso.png"
             };
 
             LanceStroll = new Pilot
@@ -190,7 +190,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Canadian",
                 TeamName = "Aston Martin",
                 Biography = "There is no such thing as too much too soon for Stroll, a teenage sensation with a wet weather predilection. One of the cool kids on the grid, Stroll was unveiled shortly after his 18th birthday by Williams – before he finished high school and got his road licence. Stroll meant business in his debut 2017 season, setting records on the way. ",
-                ImagePath = "\\wwwroot\\images\\pilots\\lancestroll.png"
+                ImagePath = "/images/pilots/lancestroll.png"
             };
 
             LewisHamilton = new Pilot
@@ -201,7 +201,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "British",
                 TeamName = "Mercedes",
                 Biography = "‘Still I Rise’ – these are the words emblazoned across the back of Lewis Hamilton’s helmet and tattooed across his shoulders, and ever since annihilating expectations with one of the greatest rookie performances in F1 history in 2007, that’s literally all he’s done: risen to the top of the all-time pole positions list.",
-                ImagePath = "\\wwwroot\\images\\pilots\\lewishamilton.png"
+                ImagePath = "/images/pilots/lewishamilton.png"
             };
 
             YukiTsunoda = new Pilot
@@ -212,7 +212,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Japanese",
                 TeamName = "AlphaTauri",
                 Biography = "In the entire history of Formula 1, no Japanese driver has ever won a World Championship Grand Prix. Red Bull certainly think so, with the youngster very much on the path to their senior team if he continues to impress as he has done over the past few years. Tsunoda's ascent to the top tier of motorsport was astonishingly rapid.",
-                ImagePath = "\\wwwroot\\images\\pilots\\yukitsunoda.png"
+                ImagePath = "/images/pilots/yukitsunoda.png"
             };
 
             NicoHulkenberg = new Pilot
@@ -223,7 +223,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "German",
                 TeamName = "Aston Martin",
                 Biography = "He’s the Superhero with the talent to become a racing superstar – if only he could get to flex his muscles with a top team. F1’s 'Hulk' has shown incredible strength and stamina as a midfield marauder for Williams, Force India, Sauber, Renault, Racing Point, Aston Martin and Haas during a career spanning back to 2010.",
-                ImagePath = "\\wwwroot\\images\\pilots\\nicohulkenberg.png"
+                ImagePath = "/images/pilots/nicohulkenberg.png"
             };
 
             KevinMagnussen = new Pilot
@@ -234,7 +234,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Danish",
                 TeamName = "Haas",
                 Biography = "Call him a lone ranger or a maverick, but Magnussen is in Formula 1 for one reason only – to race. He may be a second-generation F1 driver – following his father, Jan, onto the grid – but Magnussen’s idols are from the ‘golden era’ of Grand Prix racing when the likes of Juan Manuel Fangio and Stirling Moss risked it all for the love of the sport.",
-                ImagePath = "\\wwwroot\\images\\pilots\\kevinmagnussen.png"
+                ImagePath = "/images/pilots/kevinmagnussen.png"
             };
 
             AlexAlbon = new Pilot
@@ -245,7 +245,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Thai",
                 TeamName = "Williams",
                 Biography = "Born in London but racing under the flag of Thailand, Alexander Albon’s first word was in fact Italian. That word was Ferrari – though it was with another Italian team that he got his big F1 break. Idolising Michael Schumacher and dreaming of one day racing in Formula 1, the junior Albon was pipped to the 2016 GP3.",
-                ImagePath = "\\wwwroot\\images\\pilots\\alexalbon.png"
+                ImagePath = "/images/pilots/alexalbon.png"
             };
 
             GuanyuZhou = new Pilot
@@ -256,7 +256,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Chinese",
                 TeamName = "Alfa Romeo",
                 Biography = "China had never boasted a Grand Prix starter among its citizens – until Zhou Guanyu changed that state of affairs, after receiving the call-up to make his F1 debut for Alfa Romeo, now Kick Sauber, in 2022. The Shanghai-born racer attended his home city’s inaugural Grand Prix in 2004 at the age of five, cheering on his hero Fernando Alonso.",
-                ImagePath = "\\wwwroot\\images\\pilots\\guanyuzhou.png"
+                ImagePath = "/images/pilots/guanyuzhou.png"
             };
 
             DanielRicciardo = new Pilot
@@ -267,7 +267,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Australian",
                 TeamName = "McLaren",
                 Biography = "The self-styled “Honey Badger” is fuzzy on the outside and feisty on the inside. Drivers beware because behind Ricciardo’s laidback persona and big grin is a razor-sharp racer with a bite. The Australian combines all-out speed with impressive race craft. Never afraid to push to the limits if it means pulling off a pass, Ricciardo is a proven race-winner.",
-                ImagePath = "\\wwwroot\\images\\pilots\\danielricciardo.png"
+                ImagePath = "/images/pilots/danielricciardo.png"
             };
 
             EstebanOcon = new Pilot
@@ -278,7 +278,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "French",
                 TeamName = "Alpine",
                 Biography = "If there’s one word that dominates Esteban Ocon’s career, it’s ‘sacrifice’. Back when he was just a promising karter, Ocon’s parents sold their house, put their jobs on hold, and began a life on the road, living in a caravan and travelling from circuit to circuit to support their son’s burgeoning career.",
-                ImagePath = "\\wwwroot\\images\\pilots\\estebanocon.png"
+                ImagePath = "/images/pilots/estebanocon.png"
             };
 
             PierreGasly = new Pilot
@@ -289,7 +289,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "French",
                 TeamName = "AlphaTauri",
                 Biography = "The flying Frenchman was called up to make his 2017 debut in Malaysia in place of Daniil Kvyat and, after proving his mettle, he was named a Toro Rosso driver the following year.  A further 21 races into his fledgling career, Gasly was moved up again – this time to replace Red Bull big gun Daniel Ricciardo.",
-                ImagePath = "\\wwwroot\\images\\pilots\\pierregasly.png"
+                ImagePath = "/images/pilots/pierregasly.png"
             };
 
             ValtteriBottas = new Pilot
@@ -300,7 +300,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "Finnish",
                 TeamName = "Alfa Romeo",
                 Biography = "Learning his craft on Finnish roads of ice and snow, he was born to be a Grand Prix racer. Bottas explains that if you can drive on the frozen roads of his homeland then you can drive anywhere. Then there’s the Finnish mentality –reserved, diligent and calm the fast lane of F1 doesn’t faze him.",
-                ImagePath = "\\wwwroot\\images\\pilots\\valtteribottas.png"
+                ImagePath = "/images/pilots/valtteribottas.png"
             };
 
             LoganSargeant = new Pilot
@@ -311,7 +311,7 @@ namespace Formula1WebApplication.Infrastructure.Data.SeedDb
                 Nationality = "American",
                 TeamName = "Williams",
                 Biography = "Logan Sargeant became F1’s first American driver in almost eight years, giving the country a home favourite to cheer once more. Winner in karting, championship glory eluded Sargeant after he made the transition to single-seater racing, but pole positions and race victories in almost every category he contested underlined his raw speed and potential.",
-                ImagePath = "\\wwwroot\\images\\pilots\\logansargeant.png"
+                ImagePath = "/images/pilots/logansargeant.png"
             };
         }
 
---
+++ Formula1WebApplication.Infrastructure/Migrations/20240401170445_ImagePathsChanged.cs
@@ -0,0 +1,323 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Formula1WebApplication.Infrastructure.Migrations
+{
+    public partial class ImagePathsChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6d5800ce-d726-4fc8-83d9-d6b3ac1f591e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d6a05f26-1182-43d6-807e-a4fb33ae5be3", "AQAAAAEAACcQAAAAEKXoTjkZFBgKvdf++g3SbII3RZ6cd4+AsU40gqI0SfInDLnj2b+0+fEOHO5vC/7SWA==", "fdb12c04-7cd0-456c-bdf5-3e06ddb93aa5" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dea12856-c198-4129-b3f3-b893d8395082",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "70ed012d-ebe4-4014-b090-de7c691f3bc2", "AQAAAAEAACcQAAAAEOh2YNZzXnIb7vPs+BOxOYs9pzwVvooXHEMQ/jzVtjoSbRD8Q1r5Sw11BTLZRLvSUw==", "e289454a-f3d5-46b8-b3ea-f4248c51e135" });
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "/images/pilots/maxverstappen.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImagePath",
+                value: "/images/pilots/charlesleclerc.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "/images/pilots/sergioperez.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImagePath",
+                value: "/images/pilots/carlossainz.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImagePath",
+                value: "/images/pilots/oscarpiastri.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImagePath",
+                value: "/images/pilots/landonorris.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImagePath",
+                value: "/images/pilots/georgerussel.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImagePath",
+                value: "/images/pilots/fernandoalonso.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImagePath",
+                value: "/images/pilots/lancestroll.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImagePath",
+                value: "/images/pilots/lewishamilton.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImagePath",
+                value: "/images/pilots/yukitsunoda.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImagePath",
+                value: "/images/pilots/nicohulkenberg.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImagePath",
+                value: "/images/pilots/kevinmagnussen.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImagePath",
+                value: "/images/pilots/alexalbon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImagePath",
+                value: "/images/pilots/guanyuzhou.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImagePath",
+                value: "/images/pilots/danielricciardo.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImagePath",
+                value: "/images/pilots/estebanocon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImagePath",
+                value: "/images/pilots/pierregasly.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImagePath",
+                value: "/images/pilots/valtteribottas.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImagePath",
+                value: "/images/pilots/logansargeant.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6d5800ce-d726-4fc8-83d9-d6b3ac1f591e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e2daa778-2223-43c3-b7bc-eacf8c008b59", "AQAAAAEAACcQAAAAEAh3NH09bw4bqJYSKJ8QL2bpvA4yoADG6fElzg59g/vprRjEMS52YfeLEhCvbsWUuA==", "c4391048-353f-4129-aa19-fcf8551b2ec9" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dea12856-c198-4129-b3f3-b893d8395082",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9ecf8983-890a-42af-aa3f-1adb99a4b68b", "AQAAAAEAACcQAAAAELhYFf97FnRnDubWxvTwtd2A19oO19HKuFY/DH8Fvoy/1Nt48OBwMTIDHPffA9bHHw==", "1636cf4c-82ff-4c1e-80f8-ebdcd65239c8" });
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\maxverstappen.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\charlesleclerc.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\sergioperez.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\carlossainz.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\oscarpiastri.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\landonorris.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\georgerussel.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\fernandoalonso.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\lancestroll.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\lewishamilton.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\yukitsunoda.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\nicohulkenberg.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\kevinmagnussen.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\alexalbon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\guanyuzhou.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\danielricciardo.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\estebanocon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\pierregasly.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\valtteribottas.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pilots",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImagePath",
+                value: "\\wwwroot\\images\\pilots\\logansargeant.png");
+        }
+    }
+}
