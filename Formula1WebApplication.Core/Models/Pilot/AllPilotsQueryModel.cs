9bec136d67467a8e708a5097a2413b19524531d8
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 12:55:27 +0300

    All Pilots Query Model created.

--- Formula1WebApplication.Core/Models/Pilot/AllPilotsQueryModel.cs
+++ Formula1WebApplication.Core/Models/Pilot/AllPilotsQueryModel.cs
@@ -2,5 +2,10 @@
 {
     public class AllPilotsQueryModel
     {
+        public IEnumerable<PilotServiceModel> Pilots { get; set; } = new List<PilotServiceModel>();
+        public string SearchTerm { get; set; } = string.Empty;
+        public string NationalityFilter { get; set; } = string.Empty;
+        public string TeamFilter { get; set; } = string.Empty;
+        public string SortOrder { get; set; } = string.Empty;
     }
 }
