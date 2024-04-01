03625d4e0d2ff13893649e4e9ae82d7845b4430c
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-01 12:56:59 +0300

    "All" method implemented in Pilot Controller.

--- Formula1WebApplication/Controllers/PilotController.cs
+++ Formula1WebApplication/Controllers/PilotController.cs
@@ -1,17 +1,24 @@
-﻿using Formula1WebApplication.Core.Models.Pilot;
+﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Core.Models.Pilot;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Formula1WebApplication.Controllers
 {
     public class PilotController : BaseController
 	{
-		[HttpGet]
-		public async Task<IActionResult> All()
-        {
-			var model = new AllPilotsQueryModel();
+        private readonly IPilotService pilotService;
 
-			return View(model);
-		}
+        public PilotController(IPilotService _pilotService)
+        {
+            pilotService = _pilotService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All(string searchTerm, string nationality, string team, string sortOrder)
+        {
+            var queryModel = await pilotService.GetAllPilotsAsync(searchTerm, nationality, team, sortOrder);
+            return View(queryModel);
+        }
 
         [HttpGet]
         public async Task<IActionResult> Details()
