2c4f136b6e9dcc11960a483e87ec4d3df536bb1d
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-02 21:37:00 +0300

    Event User entity model created.

---
+++ Formula1WebApplication.Infrastructure/Data/Models/EventUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Formula1WebApplication.Infrastructure.Data.Models
+{
+    public class EventUser
+    {
+        [Comment("Event Id")]
+        public int EventId { get; set; }
+
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; } = null!;
+
+        [Comment("User Id")]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
