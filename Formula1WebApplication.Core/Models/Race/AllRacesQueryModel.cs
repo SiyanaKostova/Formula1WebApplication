e6e475e28168d08d1350f51bda693a2c596312b3
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-03 14:01:49 +0300

    Created All Races Query Model.

--- Formula1WebApplication.Core/Models/Race/AllRacesQueryModel.cs
+++ Formula1WebApplication.Core/Models/Race/AllRacesQueryModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Formula1WebApplication.Core.Models.Race
+﻿using Formula1WebApplication.Infrastructure.Pagination;
+
+namespace Formula1WebApplication.Core.Models.Race
 {
     public class AllRacesQueryModel
     {
+        public PaginatedList<RaceServiceModel> Races { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public string SearchString { get; set; } = string.Empty;
+        public string SortOrder { get; set; } = string.Empty;
     }
 }
