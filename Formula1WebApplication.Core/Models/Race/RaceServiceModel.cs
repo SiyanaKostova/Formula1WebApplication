78e17a10e9fc33cfa99580fd6bbfa66bb989d9b0
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-03 14:01:30 +0300

    Created service model for Race.

--- Formula1WebApplication.Core/Models/Race/RaceServiceModel.cs
+++ Formula1WebApplication.Core/Models/Race/RaceServiceModel.cs
@@ -1,6 +1,36 @@
-﻿namespace Formula1WebApplication.Core.Models.Race
+﻿using System.ComponentModel.DataAnnotations;
+using static Formula1WebApplication.Core.Constants.MessageConstants;
+using static Formula1WebApplication.Infrastructure.Constants.DataConstants;
+
+namespace Formula1WebApplication.Core.Models.Race
 {
     public class RaceServiceModel
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(RaceNameMaxLength,
+         ErrorMessage = MaxLengthMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(RaceLocationMaxLength,
+        MinimumLength = RaceLocationMinLength,
+         ErrorMessage = LengthMessage)]
+        public string Location { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        public int Laps { get; set; }
+
+        [StringLength(RaceCircuitInfoMaxLength,
+        MinimumLength = RaceCircuitInfoMinLength,
+         ErrorMessage = LengthMessage)]
+        public string CircuitInfo { get; set; } = string.Empty;
     }
 }
