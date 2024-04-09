693dc34a67bc0a43b02ed4d372a3c9cf3bea9b93
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-09 20:59:14 +0300

    News Article Model with Title and Description created.

---
+++ Formula1WebApplication.Core/Contracts/InterfaceModels/INewsArticleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Formula1WebApplication.Core.Contracts.InterfaceModels
+{
+    public interface INewsArticleModel
+    {
+        public string Title { get; }
+        public string Description { get; }
+    }
+}
