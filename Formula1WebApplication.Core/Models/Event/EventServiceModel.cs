a5e5c0cb1c8982467edbcaaff3e2e7f447723152
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-09 21:42:12 +0300

    IEventModel inherited in EventServiceModel.

--- Formula1WebApplication.Core/Models/Event/EventServiceModel.cs
+++ Formula1WebApplication.Core/Models/Event/EventServiceModel.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Formula1WebApplication.Core.Contracts.InterfaceModels;
+using System.ComponentModel.DataAnnotations;
 using static Formula1WebApplication.Core.Constants.MessageConstants;
 using static Formula1WebApplication.Infrastructure.Constants.DataConstants;
 
 namespace Formula1WebApplication.Core.Models.Event
 {
-    public class EventServiceModel
+    public class EventServiceModel : IEventModel
     {
         public int Id { get; set; }
 
