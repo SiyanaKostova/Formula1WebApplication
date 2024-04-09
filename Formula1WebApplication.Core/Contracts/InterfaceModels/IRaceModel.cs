36ad38c048e11aad8d03e792437e4612aaa01f08
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-09 22:15:00 +0300

    Race Model with Name and Circuit Info created.

---
+++ Formula1WebApplication.Core/Contracts/InterfaceModels/IRaceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Formula1WebApplication.Core.Contracts.InterfaceModels
+{
+    public interface IRaceModel
+    {
+        public string Name { get; }
+        public string CircuitInfo { get; }
+    }
+}
