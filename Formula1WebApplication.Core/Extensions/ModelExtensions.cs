858c7b2122122330dc86c87f5268ef83f01769ac
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-09 20:58:42 +0300

    GetNewsArticleDetails and GetDescription implemented in Model Extensions.

---
+++ Formula1WebApplication.Core/Extensions/ModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Formula1WebApplication.Core.Contracts.InterfaceModels;
+using System.Text.RegularExpressions;
+
+namespace Formula1WebApplication.Core.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetNewsArticleDetails(this INewsArticleModel newsArticle)
+        {
+            string info = newsArticle.Title.Replace(" ", "-") + GetDescription(newsArticle.Description);
+
+            info = Regex.Replace(info, @"[^a-zA-Z0-9\-]", string.Empty);
+
+            return info;
+        }
+
+        private static string GetDescription(string  description)
+        {
+            description = string.Join("-", description.Split(" ").Take(4));
+
+            return description;
+        }
+    }
+}
