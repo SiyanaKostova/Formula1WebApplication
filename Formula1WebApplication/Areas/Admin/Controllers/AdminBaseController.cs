f8461b82ca233d017d580f49e075cfddc03a9d53
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-11 00:30:06 +0300

    Admin Base Controller implemented.

---
+++ Formula1WebApplication/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static Formula1WebApplication.Core.Constants.AdministratorConstants;
+
+namespace Formula1WebApplication.Areas.Admin.Controllers
+{
+    [Area(AdminAreaName)]
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+    }
+}
