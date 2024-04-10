c21ec60be2b4ca548405b03f8a4f2a60625590cb
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-10 11:21:41 +0300

    Application User entity model created.

---
+++ Formula1WebApplication.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static Formula1WebApplication.Infrastructure.Constants.DataConstants;
+
+namespace Formula1WebApplication.Infrastructure.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(UserFirstNameMaxLength)]
+        [PersonalData]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(UserLastNameMaxLength)]
+        [PersonalData]
+        public string LastName { get; set; } = string.Empty;
+
+        public Organizer? Organizer { get; set; }
+    }
+}
