94fd272e1b572af95c3acfa21ba0bec777d7f4e0
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-10 11:47:55 +0300

    Changed Identity User for Application User in User Configuration.

--- Formula1WebApplication.Infrastructure/Data/SeedDb/Configurations/UserConfiguration.cs
+++ Formula1WebApplication.Infrastructure/Data/SeedDb/Configurations/UserConfiguration.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Formula1WebApplication.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Formula1WebApplication.Infrastructure.Data.SeedDb.Configurations
 {
-    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+	internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
     {
-        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
             var data = new SeedData();
 
-            builder.HasData(new IdentityUser[] { data.OrganizerUser, data.GuestUser });
+            builder.HasData(new ApplicationUser[] { data.OrganizerUser, data.GuestUser });
         }
     }
 }
