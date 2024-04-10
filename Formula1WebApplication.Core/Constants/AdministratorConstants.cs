0f9acf348ebf84f9cfdddf2285f3648e00408e76
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-10 23:00:03 +0300

    Administrator Constants created.

---
+++ Formula1WebApplication.Core/Constants/AdministratorConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Formula1WebApplication.Core.Constants
+{
+	public static class AdministratorConstants
+	{
+		public const string AdminRole = "Administrator";
+		public const string AdminAreaName = "Admin";
+		public const string AdminEmail = "admin@mail.com";
+		public const string UsersCacheKey = "UsersCacheKey";
+	}
+}
