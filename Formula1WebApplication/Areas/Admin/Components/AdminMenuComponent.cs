3aae119dbd36f67c669462f6c3ec9ea32808cc1b
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-11 00:30:57 +0300

    Admin Menu Component Invoke Async method implemented.

---
+++ Formula1WebApplication/Areas/Admin/Components/AdminMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Formula1WebApplication.Areas.Admin.Components
+{
+    public class AdminMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
