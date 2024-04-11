15e7c2614eec1130e48fd2cc57d810d16c6364c4
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-12 00:49:33 +0300

    "All" Users implemented in User Controller. (Admin Area)

---
+++ Formula1WebApplication/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Core.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using static Formula1WebApplication.Core.Constants.AdministratorConstants;
+
+namespace Formula1WebApplication.Areas.Admin.Controllers
+{
+    public class UserController : AdminBaseController
+    {
+        private readonly IUserService userService;
+        private readonly IMemoryCache memoryCache;
+
+        public UserController(
+            IUserService _userService,
+            IMemoryCache _memoryCache)
+        {
+            userService = _userService;
+            memoryCache = _memoryCache;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var model = memoryCache.Get<IEnumerable<UserServiceModel>>(UsersCacheKey);
+
+            if (model == null || model.Any() == false)
+            {
+                model = await userService.AllUsersAsync();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(2));
+
+                memoryCache.Set(UsersCacheKey, model, cacheOptions);
+            }
+
+            return View(model);
+        }
+    }
+}
