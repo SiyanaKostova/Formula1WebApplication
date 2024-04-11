5dbd6015d4b476ae1826539e27a42f02a432b570
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-11 23:15:09 +0300

    DashBoard added to Home Controller. (Admin Area)

--- Formula1WebApplication/Areas/Admin/Controllers/HomeController.cs
+++ Formula1WebApplication/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿namespace Formula1WebApplication.Areas.Admin.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Formula1WebApplication.Areas.Admin.Controllers
 {
 	public class HomeController : AdminBaseController
     {
-    }
+		public IActionResult DashBoard()
+		{
+			return View();
+		}
+	}
 }
