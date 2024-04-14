0f3bcd2a16fbedf56b3d50ec5cd569cd2e8fb1a1
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-14 14:17:16 +0300

    Solution for Tests added.

---
+++ Formula1WebApplication.Tests/NewsArticleServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1WebApplication.Tests
+{
+    public class NewsArticleServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
