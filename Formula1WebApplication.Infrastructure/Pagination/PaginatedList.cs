c12806f1f6dc3978fae08443e4a056386a925cce
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-14 18:21:05 +0300

    Page Size property added to Paginated List.

--- Formula1WebApplication.Infrastructure/Pagination/PaginatedList.cs
+++ Formula1WebApplication.Infrastructure/Pagination/PaginatedList.cs
@@ -5,6 +5,7 @@
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
         public int TotalCount { get; private set; }
+        public int PageSize { get; private set; }
 
         public bool HasPreviousPage => PageIndex > 1;
         public bool HasNextPage => PageIndex < TotalPages;
@@ -13,6 +14,7 @@
         {
             TotalCount = count;
             PageIndex = pageIndex;
+            PageSize = pageSize;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             AddRange(items);
         }
