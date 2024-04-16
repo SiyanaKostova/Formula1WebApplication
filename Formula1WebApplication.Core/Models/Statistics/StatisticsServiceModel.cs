e0bff800f6e8584c1c50fa057d953ecd18b24265
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-16 20:13:18 +0300

    Statistics Service Model created.

---
+++ Formula1WebApplication.Core/Models/Statistics/StatisticsServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Formula1WebApplication.Core.Models.Statistics
+{
+	public class StatisticsServiceModel
+	{
+        public int TotalPilots { get; set; }
+        public int TotalNewsArticles { get; set; }
+        public int TotalEvents { get; set; }
+        public int TotalRaces { get; set; }
+        public int TotalOrganizers { get; set; }
+    }
+}
