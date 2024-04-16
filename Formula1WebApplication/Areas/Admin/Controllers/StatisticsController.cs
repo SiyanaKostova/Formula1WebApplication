a2cc1a4e1047ee6848856926d5eb9fd602a60e7f
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-16 20:14:31 +0300

    Statistics Controller created in Admin Area.

---
+++ Formula1WebApplication/Areas/Admin/Controllers/StatisticsController.cs
@@ -0,0 +1,23 @@
+﻿using Formula1WebApplication.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formula1WebApplication.Areas.Admin.Controllers
+{
+	public class StatisticsController : AdminBaseController
+	{
+		private readonly IStatisticsService statisticsService;
+
+		public StatisticsController(IStatisticsService statisticsService)
+		{
+			this.statisticsService = statisticsService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Statistics()
+		{
+			var statistics = await statisticsService.TotalAsync();
+
+			return View(statistics); 
+		}
+	}
+}
