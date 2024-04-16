2ade0c983b9afc2af131f90ce0568ebd3510cda2
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-16 20:13:45 +0300

    Statistics Service TotalAsync() implemented.

---
+++ Formula1WebApplication.Core/Contracts/IStatisticsService.cs
@@ -0,0 +1,9 @@
+﻿using Formula1WebApplication.Core.Models.Statistics;
+
+namespace Formula1WebApplication.Core.Contracts
+{
+	public interface IStatisticsService
+	{
+		Task<StatisticsServiceModel> TotalAsync();
+	}
+}
---
+++ Formula1WebApplication.Core/Services/StatisticsService.cs
@@ -0,0 +1,45 @@
+﻿using Formula1WebApplication.Core.Contracts;
+using Formula1WebApplication.Core.Models.Statistics;
+using Formula1WebApplication.Infrastructure.Common;
+using Formula1WebApplication.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formula1WebApplication.Core.Services
+{
+	public class StatisticsService : IStatisticsService
+	{
+		private readonly IRepository repository;
+
+        public StatisticsService(IRepository _repository)
+        {
+			repository = _repository;
+        }
+
+        public async Task<StatisticsServiceModel> TotalAsync()
+		{
+			int totalPilots = await repository.AllReadOnly<Pilot>()
+				.CountAsync();
+
+			int totalNewsArticles = await repository.AllReadOnly<NewsArticle>()
+				.CountAsync();
+
+			int totalEvents = await repository.AllReadOnly<Event>()
+				.CountAsync();
+
+			int totalRaces = await repository.AllReadOnly<Race>()
+				.CountAsync();
+
+			int totalOrganizers = await repository.AllReadOnly<Organizer>()
+				.CountAsync();
+
+			return new StatisticsServiceModel()
+			{
+				TotalPilots = totalPilots,
+				TotalNewsArticles = totalNewsArticles,
+				TotalEvents = totalEvents,
+				TotalRaces = totalRaces,
+				TotalOrganizers = totalOrganizers
+			};
+		}
+	}
+}
